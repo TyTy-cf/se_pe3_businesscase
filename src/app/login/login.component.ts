ea5bf4eb614ca545d773a6442fbb6d3f3c1539bd
Kevin Tourret <kevin@drosalys.fr>
2022-07-08 13:02:31 +0200

    working form + post login_check

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -9,8 +9,11 @@ import {AbstractControl, FormControl, FormGroup, Validators} from "@angular/form
 })
 export class LoginComponent {
 
+  // private _email: string = 'tortuga4281@yahoo.fr';
+  // private _password: string = 'coucou';
   private _email: string = '';
   private _password: string = '';
+  error: string = '';
 
   formLogin: FormGroup = new FormGroup({
     email: new FormControl(
@@ -41,8 +44,13 @@ export class LoginComponent {
 
   onSubmit(): void {
     if (this.formLogin.valid) {
-      this.httpClientService.loginCheck({email: this._email, password: this._password}).subscribe((response) => {
-        console.log(response);
+      const jsonPostUser: {email: string, password: string} = {
+        email: this.getFormControl('email').value,
+        password: this.getFormControl('password').value
+      };
+
+      this.httpClientService.loginCheck(jsonPostUser).subscribe((response) => {
+          console.log(response)
       });
     }
     // Parking avec l'utilisateur
